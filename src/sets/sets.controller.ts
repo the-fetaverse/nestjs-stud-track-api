bed03a3de9eef326542635636bc885d5bb9b4947
Giannis Sporidis <ioannis.sporidis@gmail.com>
2025-09-28 17:50:31 -0400

    08. Added POST endpoint to the set.controller

--- src/sets/sets.controller.ts
+++ src/sets/sets.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SetsService } from './sets.service';
-import { Set } from './set.model';
+import type { Set } from './set.model';
 
 @Controller('sets')
 export class SetsController {
@@ -9,5 +9,24 @@ export class SetsController {
   @Get()
   getAllSets(): Set[] {
     return this.setsService.getAllSets();
+  }
+
+  @Post()
+  createSet(
+    @Body('set_num') set_num: string,
+    @Body('set_name') set_name: string,
+    @Body('set_year') set_year: number,
+    @Body('theme_id') theme_id: number,
+    @Body('num_parts') num_parts: number,
+    @Body('set_img_url') set_img_url: string,
+  ): Set {
+    return this.setsService.createSet(
+      set_num,
+      set_name,
+      set_year,
+      theme_id,
+      num_parts,
+      set_img_url,
+    );
   }
 }
