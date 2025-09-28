5719fdea0b758b529893f520ad11b9d01d0d1b66
Giannis Sporidis <ioannis.sporidis@gmail.com>
2025-09-28 12:33:42 -0400

    04. Making the setsService available in the controller

--- src/sets/sets.controller.ts
+++ src/sets/sets.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { SetsService } from './sets.service';
 
 @Controller('sets')
-export class SetsController {}
+export class SetsController {
+  constructor(private readonly setsService: SetsService) {}
+}
