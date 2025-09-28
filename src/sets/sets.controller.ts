bfbb8c33ef07b4f1948917effc5ef4f01d63bd04
Giannis Sporidis <ioannis.sporidis@gmail.com>
2025-09-28 12:53:36 -0400

    05. Initial set up for GET for all tasks

--- src/sets/sets.controller.ts
+++ src/sets/sets.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { SetsService } from './sets.service';
 
 @Controller('sets')
 export class SetsController {
   constructor(private readonly setsService: SetsService) {}
+
+  @Get()
+  getAllSets() {
+    return this.setsService.getAllSets();
+  }
 }
--- src/sets/sets.service.ts
+++ src/sets/sets.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class SetsService {}
+export class SetsService {
+  private sets = [];
+
+  getAllSets() {
+    return this.sets;
+  }
+}
